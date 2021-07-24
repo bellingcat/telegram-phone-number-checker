5827f55f3759cd99e5c34f76f7dd0d6866ad9e16
AnonCollaborator <87876455+AnonCollaborator@users.noreply.github.com>
2021-07-24 19:10:59 +0200

    Added support for CSV output, and txt file input

--- telegram-phone-validation.py
+++ telegram-phone-validation.py
@@ -7,9 +7,8 @@ import argparse
 import os
 from getpass import getpass
 
-load_dotenv()
 
-result = {}
+load_dotenv()
 
 API_ID = os.getenv('API_ID')
 API_HASH = os.getenv('API_HASH')
@@ -34,23 +33,22 @@ def get_names(phone_number):
     except:
         raise
 
-
-def user_validator():
+def user_validator(phone_numbers: list):
     '''
     The function uses the get_api_response function to first check if the user exists and if it does, then it returns the first user name and the last user name.
     '''
-    input_phones = input("Phone numbers: ")
-    phones = input_phones.split()
-    try:
-        for phone in phones:
-            api_res = get_names(phone)
-            result[phone] = api_res
-    except:
-        raise
-            
+    result = {}
+    for phone in phones:
+        api_res = get_names(phone)
+        result[phone] = api_res
+
+    return result
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Check to see if a phone number is a valid Telegram account')
+    parser.add_argument('-i', dest='input_file_path', help='Path to an optional telephone list .txt file to be used as input')
+    parser.add_argument('--tocsv', action="store_true", 
+                        help='If present, the output will be parsed as a csv. Useful for piping the output into a file [$script -i path --tocsv > output.txt]')
 
     args = parser.parse_args()
 
@@ -63,5 +61,27 @@ if __name__ == '__main__':
         except errors.SessionPasswordNeededError:
             pw = getpass('Two-Step Verification enabled. Please enter your account password: ')
             client.sign_in(password=pw)
-    user_validator()
-    print(result)
+
+    # get input from file if the argument was used, if not, get them from the user input
+    input_phones = []
+    if not args.input_file_path:
+        input_phones = input("Phone numbers: ")
+        phones = input_phones.split()
+
+    else:
+        with open(args.input_file_path, 'r') as input_file:
+            input_phones = input_file.readlines()
+        
+        # remove spaces and newlines in the phones
+        phones = [tlf.strip('\n').replace(' ','') for tlf in input_phones]
+
+    result = user_validator(phones)
+    if not args.tocsv:
+        print(result)
+
+    else:
+        csv = "telephone,username\n"
+        for phone, username in result.items():
+            csv += f"{phone},{username}\n"
+
+        print(csv)
