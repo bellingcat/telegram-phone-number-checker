3636963d1a3a87d0245a9ef37d6274c25d23fe8f
lowaCase <62002130+BDFL669@users.noreply.github.com>
2024-03-05 18:09:33 +0300

    return richer account information (#21)
    * added more fields for user information
    * Increment version to 1.1.0 (reflecting the new functionality)
    
    ---------
    
    Co-authored-by: Galen Reich <54807169+GalenReich@users.noreply.github.com>

--- telegram_phone_number_checker/main.py
+++ telegram_phone_number_checker/main.py
@@ -30,12 +30,22 @@ def get_names(client: TelegramClient, phone_number: str) -> dict:
             # The response from DeleteContactsRequest contains more information than from ImportContactsRequest
             del_user = client(functions.contacts.DeleteContactsRequest(id=[users[0].get('id')]))
             user  = del_user.to_dict().get('users')[0]
+            user_was_online = user.get('status', {}).get('was_online')
             # getting more information about the user
             result.update({
                 "id": user.get('id'),
                 "username": user.get('username'),
                 "first_name": user.get('first_name'),
-                "last_name": user.get('last_name')
+                "last_name": user.get('last_name'),
+                "fake" : user.get('fake'),
+                "verified" : user.get('verified'),
+                "premium" : user.get('premium'),
+                "mutual_contact" : user.get('mutual_contact'),
+                "bot" : user.get('bot'),
+                "bot_chat_history" : user.get('bot_chat_history'),
+                "restricted" : user.get('restricted'),
+                "restriction_reason" : user.get('restriction_reason'),
+                "user_was_online": user_was_online.strftime("%Y-%m-%d %H:%M:%S %Z") if user_was_online else None
                 })
         else:
             result.update({"error": f'This phone number matched multiple Telegram accounts, which is unexpected. Please contact the developer: contact-tech@bellingcat.com'})
